f130c554ac9c41bbb6aceae29657f04ec81cafcf
AnOpenSauceDev <119765865+AnOpenSauceDev@users.noreply.github.com>
2023-10-15 10:29:24 +1000

    why did these changes not push

--- src/main/java/com/github/anopensaucedev/toastkiller/ToastKiller.java
+++ src/main/java/com/github/anopensaucedev/toastkiller/ToastKiller.java
@@ -1,4 +1,44 @@
 package com.github.anopensaucedev.toastkiller;
 
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.toast.Toast;
+import net.minecraft.client.toast.ToastManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ToastManager.class)
 public class ToastKiller {
+
+    @Inject(method = "draw",at = @At("HEAD"),cancellable = true)
+    public void killToasts(DrawContext context, CallbackInfo ci){
+        //Debug.Log("Killed a toast that tried to draw");
+        ci.cancel();
+    }
+
+    /**
+     * @author AnOpenSauceDev
+     * @reason forcefully overwrite toast behaviour
+     */
+    @Overwrite
+    public void add(Toast toast){
+        return;
+    }
+
+
+    @Mixin(targets = "net.minecraft.client.toast.ToastManager$Entry")
+    static class Entry<T extends Toast> { // inner class of ToastManager
+
+        /**
+         * @author AnOpenSauceDev
+         * @reason prevent toasts rendering on the client
+         */
+        @Overwrite
+        public boolean draw(int x, DrawContext context) { // lie about drawing
+            return true;
+        }
+    }
+
 }
